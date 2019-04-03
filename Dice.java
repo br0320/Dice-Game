be63fc70ac74cd09e2733fd6f894277562bfb4bb
BR <41482820+br0320@users.noreply.github.com>
2019-04-03 13:52:36 -0700

    Update Dice.java
    Updated the way some of the output is displayed

--- Dice.java
+++ Dice.java
@@ -1,13 +1,13 @@
 // Make a game where there are two 6-sided dice and the player has 3 attempts to guess the total roll of both dice.
-// Test Change 20181113
-
+// 11/13/2018 - Test Change
 // 04/03/2019 - Updated the program to ask for the number of dice to roll.
+// 04/03/2019 - Updated the way some of the output is displayed
 
 import java.util.*;
 
-public class Dice { // Start of class
+public class Dice { 
 	
-	public static void main(String[] args) { // Start of main	
+	public static void main(String[] args) { 	
 		Scanner scan = new Scanner(System.in);
 		Random rand = new Random();
 		
@@ -20,10 +20,15 @@ public class Dice { // Start of class
 		int howManyDice = scan.nextInt(); //Get the user input of the number of dice to roll
 		int dicetotal = 0;	
 	
-		System.out.println("What is your guess?");	
-		System.out.println(" ");
-		
+		System.out.println("What is your guess?"); 		
 		int guess = scan.nextInt(); // Get the input of the number the user guessed
+		
+		System.out.println(" ");
+		System.out.println("Your guess is " + guess + "!" );	
+		System.out.println(" ");
+		System.out.println("Let's see how you did!");
+		System.out.println(" ");		
+	
 		
 			for (int x = 0; x<howManyDice; x++) { // For loop to get the random dice rolls for the amount of dice the user inputted
 				int dice = rand.nextInt(6) + 1; 
@@ -33,12 +38,8 @@ public class Dice { // Start of class
 				
 			}
 			
-			System.out.println("Your guess is " + guess + "!" );	
-			System.out.println(" ");
-			System.out.println("Let's see how you did!");
-			System.out.println(" ");		
 			System.out.println("The sum of the " +howManyDice + " dice is " +dicetotal+ "!");
-			System.out.println(" ");	
+			System.out.println(" ");
 			
 		
 			if (guess == dicetotal) { // If loop to check if the user's guess was correct				
